e557a6d90263be1a7335e72dbdb9e68d0e9f7b8d
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 06:45:22 +0530

    server Started done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+
+const app = express()
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Hey")
+})
+
+
+app.get("/contact",(req,res)=>{
+    res.send(" contact is here")
+})
+
+app.listen(8080,()=>{
+    console.log("Server Started successfully")
+})
